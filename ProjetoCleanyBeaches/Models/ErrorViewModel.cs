3efb1f74d2ed919d78d316150d1724404f9a87b8
Caetano-Ventura-Coutinho <currytano@gmail.com>
2024-06-20 19:48:58 -0300

    projeto finalizado

---
+++ ProjetoCleanyBeaches/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoCleanyBeaches.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
